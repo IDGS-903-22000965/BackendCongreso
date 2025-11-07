8df65bdb498e3806df8463e55e138a70f6113b56
Diego Cortez <dcortez@cartomicro.com.mx>
2025-11-06 21:30:35 -0600

    Cambio de conexion

--- obj/Debug/net9.0/CongresoTIC.API.AssemblyInfo.cs
+++ obj/Debug/net9.0/CongresoTIC.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CongresoTIC.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a278d2446b410c2aaca75b3757ee37abcbaf64c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CongresoTIC.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CongresoTIC.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
